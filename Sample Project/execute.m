eae53bb3da7c2307c4d62e96a70a8b96826552a4
AbhilashG97 <abhilash.ag1997@gmail.com>
2018-12-27 14:30:49 +0530

    Sample Project: Buggy Code 1.0

--- Sample Project/execute.m
+++ Sample Project/execute.m
@@ -5,92 +5,64 @@
     input file is found in /data folder
 %}
 
-s = load('data/data.mat');
-figure;
-p=plot(s.val);
+s = load('data/seeg.mat');
+x = s.xx;
+
+original_signal = x(30, :); % original signal
+filtered_signal = highpass(original_signal, (50/(256/2))); % filtered-signal
+
+subplot(1,2,1);
+plot(original_signal);
 grid on;
-title('EEG Signal');
-fs = 500;
-% Sampling frequency
-N=length(s);
- 
-waveletFunction = 'db8';
-                [C,L] = wavedec(s.val,8,waveletFunction);
-       
-                cD1 = detcoef(C,L,1);
-                cD2 = detcoef(C,L,2);
-                cD3 = detcoef(C,L,3);
-                cD4 = detcoef(C,L,4);
-                cD5 = detcoef(C,L,5); %GAMA
-                cD6 = detcoef(C,L,6); %BETA
-                cD7 = detcoef(C,L,7); %ALPHA
-                cD8 = detcoef(C,L,8); %THETA
-                cA8 = appcoef(C,L,waveletFunction,8); %DELTA
-                D1 = wrcoef('d',C,L,waveletFunction,1);
-                D2 = wrcoef('d',C,L,waveletFunction,2);
-                D3 = wrcoef('d',C,L,waveletFunction,3);
-                D4 = wrcoef('d',C,L,waveletFunction,4);
-                D5 = wrcoef('d',C,L,waveletFunction,5); %GAMMA
-                D6 = wrcoef('d',C,L,waveletFunction,6); %BETA
-                D7 = wrcoef('d',C,L,waveletFunction,7); %ALPHA
-                D8 = wrcoef('d',C,L,waveletFunction,8); %THETA
-                A8 = wrcoef('a',C,L,waveletFunction,8); %DELTA
-                
-                Gamma = D5;
-                figure; subplot(5,1,1); plot(1:1:length(Gamma),Gamma);title('GAMMA');
-               
-                Beta = D6;
-                subplot(5,1,2); plot(1:1:length(Beta), Beta); title('BETA');
-                
-                
-                Alpha = D7;
-                subplot(5,1,3); plot(1:1:length(Alpha),Alpha); title('ALPHA'); 
-                
-                Theta = D8;
-                subplot(5,1,4); plot(1:1:length(Theta),Theta);title('THETA');
-                D8 = detrend(D8,0);
-                
-                 Delta = A8;
-                %figure, plot(0:1/fs:1,Delta);
-                subplot(5,1,5);plot(1:1:length(Delta),Delta);title('DELTA');
-D5 = detrend(D5,0);
-xdft = fft(D5);
-freq = 0:N/length(D5):N/2;
-xdft = xdft(1:length(D5)/2+1);
-figure;subplot(511);plot(freq,abs(xdft));title('GAMMA-FREQUENCY');
-[~,I] = max(abs(xdft));
-fprintf('Gamma:Maximum occurs at %3.2f Hz.\n',freq(I));
-D6 = detrend(D6,0);
-xdft2 = fft(D6);
-freq2 = 0:N/length(D6):N/2;
-xdft2 = xdft2(1:length(D6)/2+1);
-% figure;
-subplot(512);plot(freq2,abs(xdft2));title('BETA');
-[~,I] = max(abs(xdft2));
-fprintf('Beta:Maximum occurs at %3.2f Hz.\n',freq2(I));
-D7 = detrend(D7,0);
-xdft3 = fft(D7);
-freq3 = 0:N/length(D7):N/2;
-xdft3 = xdft3(1:length(D7)/2+1);
-% figure;
-subplot(513);plot(freq3,abs(xdft3));title('ALPHA');
-[~,I] = max(abs(xdft3));
-fprintf('Alpha:Maximum occurs at %f Hz.\n',freq3(I));
-         
-                
- 
-xdft4 = fft(D8);
-freq4 = 0:N/length(D8):N/2;
-xdft4 = xdft4(1:length(D8)/2+1);
-% figure;
-subplot(514);plot(freq4,abs(xdft4));title('THETA');
-[~,I] = max(abs(xdft4));
-fprintf('Theta:Maximum occurs at %f Hz.\n',freq4(I));
-A8 = detrend(A8,0);
-xdft5 = fft(A8);
-freq5 = 0:N/length(A8):N/2;
-xdft5 = xdft5(1:length(A8)/2+1);
-% figure;
-subplot(515);plot(freq3,abs(xdft5));title('DELTA');
-[~,I] = max(abs(xdft5));
-fprintf('Delta:Maximum occurs at %f Hz.\n',freq5(I));
+title('Stereo EEG without Notch filter');
+xlabel('Time(seconds)'), ylabel('Amplitude');
+
+subplot(1,2,2);
+plot(filtered_signal);
+grid on;
+title('Stereo EEG with Notch filter applied');
+xlabel('Time(seconds)'), ylabel('Amplitude');
+
+result = [0, 1:263228]; % result matrix
+
+%{
+    Mean
+%}
+
+for i = 1:263228 
+    temp = x(i,:);
+    result(1, i) = mean(temp); 
+end
+
+mean_filtered = mean(filtered_signal);
+mean_original = mean(original_signal);
+
+fprintf('The mean of the filtered signal: %f \n', mean_filtered);
+fprintf('The mean of the original signal: %f \n\n', mean_original);
+
+%{
+    Standard Deviation
+%}
+sd_filtered = std(filtered_signal);
+sd_original = std(original_signal);
+
+fprintf('The standard deviation of the filtered signal: %f \n', sd_filtered);
+fprintf('The standard deviation of the original signal: %f \n\n', sd_original);
+
+%{
+    Skewness 
+%}
+skewness_filtered = skewness(filtered_signal);
+skewness_original = skewness(original_signal);
+
+fprintf('The skewness of the filtered signal: %f \n', skewness_filtered);
+fprintf('The skewness of the original signal: %f \n\n', skewness_original);
+
+%{
+    Kurtosis
+%}
+kurtosis_filtered = kurtosis(filtered_signal); 
+kurtosis_original = kurtosis(original_signal);
+
+fprintf('The kurtosis of the filtered signal %f: \n', kurtosis_filtered);
+fprintf('The kurtosis of the original signal %f: \n\n', kurtosis_original);
